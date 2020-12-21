ea2e41b8853ead2898344cd9ad65a3af991cbe9d
vinci0 <armaghanurrasool@yahoo.com>
2020-12-21 16:02:20 +0500

    Changes made to actions.py to include recommendation part Also some changes to stories.yml to include the path to action_get_recommendation_interest Beside that added the action to domain file More changes needed for proper functionality ; it is just demo of possibility

--- Backend/BroBots/BroBot v1/chatbot/actions/actions.py
+++ Backend/BroBots/BroBot v1/chatbot/actions/actions.py
@@ -4,6 +4,9 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
 
+from googlesearch import search
+from youtubesearchpython import SearchVideos
+from random import randrange
 
 class InitialForm(FormAction):
 
@@ -47,3 +50,41 @@ class InitialForm(FormAction):
                 self.from_text(intent="inform_today_goal"),
             ]
         }
+
+# recommendation using the user interest
+class recommendation(Action):
+    def name(self)->Text:
+        return ("action_get_recommendation_interest");
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+
+        typeOfCategory = "music"
+        typeOfCategory = typeOfCategory.lower()
+        interestTopic = "ali zafar"
+
+        max_searches = 10
+        number = randrange(max_searches)
+
+        link=""
+
+        # for searching on google related to articles , book , news
+        if typeOfCategory == "article" or typeOfCategory == 'book' or typeOfCategory == 'news':
+            query = "'" + interestTopic + "'" + " " + typeOfCategory
+            gen = list(search(query, num=max_searches, start=0, stop=max_searches, tld='com'))
+            link = gen[number]
+
+        # for searching on youtube related to music , videos
+        elif typeOfCategory == 'music' or typeOfCategory == 'videos':
+            result = SearchVideos(interestTopic, offset=0, mode="dict", max_results=max_searches)
+            link = result.result()['search_result'][number]['link']
+            # print(result.result()['search_result'][number]['title'])
+        else:
+            print("Sorry No Recommendation Can be Suggested At The Moment.Category should be in [article,book,news,"
+                  "music,videoes]")
+        dispatcher.utter_message("Here is something for you:\n" +link);
+        return [];
